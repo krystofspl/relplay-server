2d8f54822cc9c057c328164da8eb99837fe53081
krystofspl <krystofspl@gmail.com>
2016-12-12 13:38:18 +0100

    Add albums-albums graph actions

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ global.db = require('seraph')({
   pass: 'password'
 })
 //global.libraryPath = '/home/krystof/Code/School/graph_music_library/library/'
-global.libraryPath = '/mnt/G/Hudba/Wayne Shorter'
+global.libraryPath = '/mnt/G/Hudba/Led Zeppelin'
 var fs = require('fs')
 var path = require('path')
 
@@ -141,6 +141,20 @@ app.get('/graphs/artist-albums-graph', (req, res) => {
   })
 })
 
+app.get('/graphs/albums-albums-graph', (req, res) => {
+  // Query for (album)<-[similarity]-(album)
+  var query = '\
+    MATCH (album:Album) \
+    WITH (album) \
+    OPTIONAL MATCH (album)-[:SIMILAR_TO]->(album2:Album) \
+    RETURN ID(album) as album, collect(ID(album2)) as rels \
+    '
+  db.query(query, {}, (err, result) => {
+    console.log(err)
+    res.json(result)
+  })
+})
+
 app.get('/graphs/artists-artists-graph', (req, res) => {
   // Query for (artist)<-[similarity]-(artist)
   var query = '\
@@ -254,6 +268,39 @@ app.get('/rescan', function (req, res) {
     console.log(err)
   }
   res.send('scanned')
+})
+
+// TODO change to URI
+app.post('/relationships/album-similarity/add', function (req, res) {
+  var edge = req.body.edge
+  var album1 = parseInt(edge.from)
+  var album2 = parseInt(edge.to)
+  if (album1 && album2) {
+    db.relate(album1, 'SIMILAR_TO', album2, (err, result) => {
+      // TODO err
+      console.log(err)
+      res.status(200).json(result)
+    })
+  }
+})
+
+// TODO change to DELETE+URI
+app.post('/relationships/album-similarity/delete', function (req, res) {
+  var edge = req.body.edge
+  var album1 = parseInt(edge.from)
+  var album2 = parseInt(edge.to)
+  if (album1 && album2) {
+    // TODO err
+    var query = ' \
+      MATCH (album1:Album)-[r:SIMILAR_TO]-(album2:Album) \
+      WHERE (ID(album1) = {id1} AND ID(album2) = {id2}) OR (ID(album1) = {id2} AND ID(album2) = {id1}) \
+      DELETE r \
+    '
+    db.query(query, {id1: album1, id2: album2}, (err, result) => {
+      console.log(err)
+      res.status(200).json(result)
+    })
+  }
 })
 
 // TODO change to URI
