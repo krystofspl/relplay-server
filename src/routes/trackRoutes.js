18311d25b888b4297adfcf3859130ab19e00b1c4
krystofspl <krystofspl@gmail.com>
2017-02-17 16:39:25 +0100

    Add simple track file route

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -1,10 +1,20 @@
 var Album = require('../models/Album.js')
 var Track = require('../models/Track.js')
+var path = require('path')
 
 app.get('/tracks', function (req, res) {
   Track.compose(Album, 'album', 'HAS_ALBUM')
   Track.findAll(function (err, tracks) {
     tracks.forEach(track => {if (track.album) track.album = track.album.id})
     res.json(tracks)
+  })
+})
+
+app.get('/tracks/:id/file.mp3', function (req, res) {
+  sync.fiber(() => {
+    var track = sync.await(Track.read(parseInt(req.params.id), sync.defer()))
+    var filePath = track.filePath
+    res.set('Content-Type', 'audio/mpeg3')
+    res.sendFile(path.join(global.libraryPath, filePath))
   })
 })
